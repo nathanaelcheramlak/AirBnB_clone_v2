3af463157f84ad3e4fb7af65935a842f9e781457
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2023-11-22 23:35:38 +0300

    minor changes to filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,14 +9,24 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """ Returns the dictionary __objects """
         return FileStorage.__objects
+    
+    def delete(self, obj=None):
+        """deletes obj from __objects if it's inside
+        Args:
+            obj: given object
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        if obj:
+            del self.__objects[key]
 
     def new(self, obj):
-        """ Sets in __objects the obj with key <obj class name >.id """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """  deletes obj from __objects if it’s inside - if obj is equal to None """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        if obj:
+            del self.__objects[key]
 
     def save(self):
         """ Serializes __objects to the JSON file """
